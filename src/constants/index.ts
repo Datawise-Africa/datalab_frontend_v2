6cb943fd90556e90ad4e1f9b95fe82565c853ff8
BrianOdhiambo <brianodhiambo530@gmail.com>
2025-06-28 12:12:02 +0300

    Fixed production backend url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -69,5 +69,5 @@ export const cookieOptions = {
 // export const REACT_PUBLIC_API_HOST = "http://localhost:8000"
 
 export const REACT_PUBLIC_API_HOST =
-  'https://d1a9-102-211-145-241.ngrok-free.app';
-// 'https://backend.datawiseafrica.com';
+  // 'https://d1a9-102-211-145-241.ngrok-free.app';
+'https://backend.datawiseafrica.com';
