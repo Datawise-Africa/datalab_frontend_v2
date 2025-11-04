f7c057158d3fa286c46b392eca2db3cdb6c27719
Winny <winnychelangat28@gmail.com>
2025-11-04 12:48:14 +0300

    Published dashboard

--- src/components/Dashboards/AfyakenDashboard.tsx
+++ src/components/Dashboards/AfyakenDashboard.tsx
@@ -5,12 +5,10 @@ const AfyakenDashboard = () => {
       {/* <h2 className="text-2xl font-semibold mb-6 text-center">AfyaKen Dashboard</h2> */}
 
       {/* <div className="w-full max-w-6xl rounded-2xl overflow-hidden shadow-lg"> */}
-        <iframe 
-          title="AfyaKen Health Dashboard"
-          src="https://app.powerbi.com/reportEmbed?reportId=dc1ec3a3-f1fe-4caf-8b23-203f8adcab0a&autoAuth=true&ctid=4eb15420-3f9b-4655-90c4-6b6c2a7d21d8"
-          allowFullScreen
-          className="w-full  min-h-screen border-0 rounded-2xl"
-        />
+       <iframe className="w-full  min-h-screen border-0 rounded-2xl"
+        title="Health Dashboard"
+        width="600" height="373.5" 
+        src="https://app.powerbi.com/view?r=eyJrIjoiZjBmZDFiMWEtZjEyOS00ODcwLThkNzMtNmJjMTQ1YjUzZmY5IiwidCI6IjRlYjE1NDIwLTNmOWItNDY1NS05MGM0LTZiNmMyYTdkMjFkOCJ9" frameBorder="0" allowFullScreen></iframe>
       {/* </div> */}
     </section>
   );
